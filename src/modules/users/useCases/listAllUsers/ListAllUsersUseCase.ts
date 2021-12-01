3905ea0bb7fc6a22d186a6881169f6fab365012a
Caroline Santos de Abreu <carols_a@live.com>
2021-12-01 15:09:57 -0300

    first commit

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,22 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  // eslint-disable-next-line prettier/prettier
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const users = this.usersRepository.list();
+
+    const userAdmin = users.find((user) => user.id === user_id);
+
+    if (userAdmin.admin !== true) {
+      throw new Error("Not authorized!");
+    }
+
+    if (!userAdmin) {
+      throw new Error("User don't exists");
+    }
+    return users;
   }
 }
 
